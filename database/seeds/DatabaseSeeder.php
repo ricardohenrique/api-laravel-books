8cf0f9bd57fef7e5cd610e3e5ee4976469009d72
ricardohenrique <ricardo.hb@outlook.com>
2019-01-31 03:08:45 -0300

    register data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            DataDefaultSeeder::class,
             AuthorTableSeeder::class,
             DisciplineTableSeeder::class,
             LevelTableSeeder::class,
