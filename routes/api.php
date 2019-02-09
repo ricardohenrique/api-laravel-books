66e704c61c05785a76b4931703cea39445031518
ricardohenrique <ricardo.hb@outlook.com>
2019-02-09 14:27:04 -0300

    init routes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resource('levels', 'LevelController');
+Route::resource('disciplines', 'DisciplineController');
+Route::resource('authors', 'AuthorsController');
+Route::resource('books', 'BookController');
