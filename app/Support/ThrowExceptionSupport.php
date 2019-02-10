a453e1f4381290002f9362266acf5eedc817ae0f
ricardohenrique <ricardo.hb@outlook.com>
2019-02-09 23:16:53 -0300

    removed exception

--- app/Support/ThrowExceptionSupport.php
+++ app/Support/ThrowExceptionSupport.php
@@ -4,8 +4,6 @@ namespace App\Support;
 
 use App\Models\ModelInterface;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
-use App\Exceptions\FileNotFoundException;
-use Storage;
 
 trait ThrowExceptionSupport
 {
@@ -21,22 +19,6 @@ trait ThrowExceptionSupport
             throw new ModelNotFoundException(sprintf(
                 'The resource \'%s\' was not found.',
                 $id
-            ));
-        }
-    }
-
-    /**
-     * @param string $fileName
-     * @param string $drive
-     * @throws FileNotFoundException
-     * @return void
-     */
-    public static function validateFileExist(string $fileName, string $drive): void
-    {
-        if (!Storage::disk($drive)->exists($fileName)) {
-            throw new FileNotFoundException(sprintf(
-                'The file \'%s\' was not found.',
-                $fileName
             ));
         }
     }
